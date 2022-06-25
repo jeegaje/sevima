fb86c0333d9e5b23277683cd7d41fccc705fd7ab
jeegaje <anggajiyanfajar@gmail.com>
2022-06-25 22:38:57 +0700

    add model lesson

--- application/app/Models/Classroom.php
+++ application/app/Models/Classroom.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
+use App\Models\Lesson;
 
 class Classroom extends Model
 {
@@ -12,5 +13,9 @@ class Classroom extends Model
 
     public function user(){
       return $this->belongsTo(User::class);
+    }
+
+    public function lesson(){
+      return $this->hasMany(Lesson::class);
     }
 }
